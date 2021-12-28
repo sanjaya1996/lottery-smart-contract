62c959dbe3f3b5835cef5850defbc3d79cbe9d1e
Sanzay Dahal <sanzaydahal12345@gmail.com>
2021-12-29 00:20:39 +1100

    End to end test- send money to the winner and reset the players array

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -85,4 +85,29 @@ describe('Lottery Contract', () => {
 
     assert.equal('fail', executed);
   });
+
+  it('sends money to the winner and resets the players array', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether'),
+    });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+    const difference = finalBalance - initialBalance;
+
+    assert(difference > web3.utils.toWei('1.8', 'ether'));
+
+    const players = await lottery.methods
+      .getPlayers()
+      .call({ from: accounts[0] });
+    assert.equal(players.length, 0);
+
+    const contractBalance = await web3.eth.getBalance(lottery.options.address);
+    assert.equal(contractBalance, 0);
+  });
 });
