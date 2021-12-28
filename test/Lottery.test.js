fc08aa33fbeb772068d212e73c9fbeef8296686a
Sanzay Dahal <sanzaydahal12345@gmail.com>
2021-12-28 16:49:56 +1100

    Get started with testing- Lottery deployment test

---
+++ test/Lottery.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const { abi, evm } = require('../compile');
+
+let accounts;
+let lottery;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  lottery = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('Lottery Contract', () => {
+  it('deployes a contract', () => {
+    assert.ok(lottery.options.address);
+  });
+});
