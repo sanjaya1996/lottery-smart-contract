8de11c95a71ef7ad7bdcc275647280dba1376c8f
Sanzay Dahal <sanzaydahal12345@gmail.com>
2021-12-28 17:21:55 +1100

    Try-catch assertions to test minimimum ether amount require to enter into the lottery

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -56,4 +56,16 @@ describe('Lottery Contract', () => {
     assert.equal(accounts[2], players[2]);
     assert.equal(3, players.length);
   });
+
+  it('requires a minimum 0.01 of ether to enter', async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 0,
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
 });
