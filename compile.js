5064dfe41b258649f6a9b359a038a6edf47b980c
Sanzay Dahal <sanzaydahal12345@gmail.com>
2021-12-28 15:00:43 +1100

    Add compile file to compile solidity code

---
+++ compile.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf-8');
+
+const lottery = {
+  language: 'Solidity',
+  sources: {
+    'Lottery.sol': {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
+    },
+  },
+};
+
+module.exports = JSON.parse(solc.compile(JSON.stringify(lottery))).contracts[
+  'Lottery.sol'
+].Lottery;
