e8615ddd220d9e8d55a2e23cf5f82f398350d5a5
Sanzay Dahal <sanzaydahal12345@gmail.com>
2021-12-28 17:12:43 +1100

    Test a single and multiple users entering the lottery

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -20,4 +20,40 @@ describe('Lottery Contract', () => {
   it('deployes a contract', () => {
     assert.ok(lottery.options.address);
   });
+
+  it('allows one account to enter', async () => {
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[0], value: web3.utils.toWei('0.02', 'ether') });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
+  });
+
+  it('allows multiple accounts to enter', async () => {
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[0], value: web3.utils.toWei('0.02', 'ether') });
+
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[1], value: web3.utils.toWei('0.02', 'ether') });
+
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[2], value: web3.utils.toWei('0.02', 'ether') });
+
+    const players = await lottery.methods
+      .getPlayers()
+      .call({ from: accounts[0] });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+    assert.equal(3, players.length);
+  });
 });
